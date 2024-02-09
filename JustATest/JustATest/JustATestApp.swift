70fb6949588b9c30c0f1be16d4e7fc9238a7785e
Sinuar <sinuar@Sinuars-MacBook-Pro.local>
2024-02-09 15:31:27 -0600

    A test on how to sync a local folder with Github

---
+++ JustATest/JustATest/JustATestApp.swift
@@ -0,0 +1,17 @@
+//
+//  JustATestApp.swift
+//  JustATest
+//
+//  Created by Sinuar on 09/02/24.
+//
+
+import SwiftUI
+
+@main
+struct JustATestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
